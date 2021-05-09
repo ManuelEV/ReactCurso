201db33d8d3ced5da02b6945aee9bbbd7fdc5291
Manuel <manuelespv@gmail.com>
2021-05-08 23:20:24 -0400

    enzyme setup

--- 01-basico/counter-app/src/tests/PrimeraApp.test.js
+++ 01-basico/counter-app/src/tests/PrimeraApp.test.js
@@ -1,15 +1,28 @@
+import {render} from '@testing-library/react'
 import React from 'react';
 import PrimeraApp from '../PrimeraApp'
-import {render} from '@testing-library/react'
+import {shallow} from 'enzyme';
+import '@testing-library/jest-dom';
 
 describe('Pruebas en PrimeraApp', () => {
 
+    // JEST WAY
+    /*
     test('debe de mostrar el mensaje "Hola soy Juan"', () => {
 
         const saludo = 'Hola, soy Juan'
         const {getByText} = render ( <PrimeraApp saludo= {saludo}/> );
 
         expect(getByText(saludo)).toBeInTheDocument();
+    })
+    */
+
+
+    test('debe de mostrar <PrimeraApp /> correctamente', () => {
+        const saludo = 'Hola, soy Juan';
+        const wrapper = shallow(<PrimeraApp saludo= {saludo}/>);
+
+        expect(wrapper).toMatchSnapshot();
     })
 
 })
